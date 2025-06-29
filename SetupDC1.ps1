40b9593a77c7d76630b792fd05513202d5108551
bwidom <165341180+bwidom@users.noreply.github.com>
2025-06-29 16:37:14 -0400

    Update and rename Test.ps1 to SetupDC1.ps1

--- SetupDC1.ps1
+++ SetupDC1.ps1
@@ -1,2 +1,3 @@
+Set-TimeZone -Id "Eastern Standard Time"
 Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName 'MyDomain.com' -SafeModeAdministratorPassword $(ConvertTo-SecureString 'Adminpassword1' -AsPlainText -Force) -Confirm:$false
