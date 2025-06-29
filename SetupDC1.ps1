09b177977f8feee86ca8837c841f733ec83899c9
bwidom <165341180+bwidom@users.noreply.github.com>
2025-06-29 17:45:01 -0400

    Update SetupDC1.ps1

--- SetupDC1.ps1
+++ SetupDC1.ps1
@@ -1,6 +1,19 @@
 Set-TimeZone -Id "Eastern Standard Time"
 #Make server discoverable on network
 netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
+
+#Install Winget
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager -AllUsers
+Write-Host "Done."
+
+#Install git
+winget install --id Git.Git -e --source winget
+
 #Install Active Directory
 Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
 #Setup Domain Forest
