e7265a3ac939b6129e61488cda10aacf03d0418d
bwidom <165341180+bwidom@users.noreply.github.com>
2025-06-29 16:41:10 -0400

    Update SetupDC1.ps1

--- SetupDC1.ps1
+++ SetupDC1.ps1
@@ -1,5 +1,7 @@
 Set-TimeZone -Id "Eastern Standard Time"
 #Make server discoverable on network
 netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
+#Install Active Directory
 Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
+#Setup Domain Forest
 Install-ADDSForest -DomainName 'MyDomain.com' -SafeModeAdministratorPassword $(ConvertTo-SecureString 'Adminpassword1' -AsPlainText -Force) -Confirm:$false
